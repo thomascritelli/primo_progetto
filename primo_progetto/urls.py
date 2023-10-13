b51c97f711ad3f26202696aed11c27c11037ef87
thomascritelli <thomas.critelli@alessandrinimainardi.edu.it>
2023-10-13 12:17:27 +0200

    Add Index progetto

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from primo_progetto.views import *
 
+
+app_name = "primo_progetto"
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("prima_app.urls", namespace= "prima_app"))
+    path('', include("primo_progetto.urls", namespace= "primo_progetto")),
+    path('', index, name = 'index')
 ]
