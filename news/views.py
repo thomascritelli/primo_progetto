6b075d1cc963b532f071d9c0455ba9213b39b000
thomascritelli <thomas.critelli@alessandrinimainardi.edu.it>
2023-11-17 09:23:07 +0100

    update

--- news/views.py
+++ news/views.py
@@ -45,3 +45,5 @@ def articoloDetailView(request, pk):
     articolo = get_object_or_404(Articolo, pk = pk)
     context = {"articolo": articolo}
     return render(request, "articolo_detail.html", context)
+
+# def listaArticoli():
