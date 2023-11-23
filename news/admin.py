d03b505719ca19360f2e7fb1d60ba418357227f0
thomascritelli <thomas.critelli@alessandrinimainardi.edu.it>
2023-11-23 08:39:23 +0100

    fix news

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Articolo, Giornalista
 # Register your models here.
+
+admin.site.register(Articolo)
+admin.site.register(Giornalista)
--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ app_name = 'news'
 urlpatterns = [
     path('homeview', homeview, name = 'homeview'),
     path('articoli/<int:pk>', articoloDetailView, name = 'articolo_detail'),
+    path('lista_articoli/<int:pk>', lista_articoli, name = 'lista_articoli'),
 ]
